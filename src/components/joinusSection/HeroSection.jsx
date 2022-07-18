2dfe7f7c201f7f309d03f7d0ff04c412b1bba90b
Ayush <62958067+Ayush-v@users.noreply.github.com>
2022-07-18 16:27:00 +0530

    working

--- src/components/joinusSection/HeroSection.jsx
+++ src/components/joinusSection/HeroSection.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 const HeroSection = () => {
   return (
     <>
-      <div className="mb-16 max-w-screen-xl mx-auto px-10 sm:pb-8 lg:pb-0 sm:pt-24 py-14 bg-gradient-to-br from-[#3675F0] to-[#2651A6] clipping rounded-b-3xl text-white flex flex-col relative overflow-hidden md:flex-row gap-9">
+      <div className="my-16 max-w-screen-xl mx-auto px-10 sm:pb-8 lg:pb-0 sm:pt-24 py-14 bg-gradient-to-br from-[#3675F0] to-[#2651A6] clipping rounded-b-3xl text-white flex flex-col relative overflow-hidden md:flex-row gap-9">
         <div className="md:my-8">
           <p className="opacity-[0.8]">intern's welcome</p>
           <h1 className="text-5xl lg:text-6xl mb-8">
